ddab772c6cb16937b0fbdadb006a26b720d96450
Yann BRONNER <yann.bronner@gmail.com>
2022-12-13 09:58:33 +0100

    W3C correction

--- src/showBestMovie.js
+++ src/showBestMovie.js
@@ -1,7 +1,9 @@
 export default function showBestMovie(bestMovieJson) {
+    const leftBestMovie = document.querySelector(".left-best-movie");
+    const bestMovieTitle = document.createElement("h1");
+    bestMovieTitle.textContent = bestMovieJson.title;
+    leftBestMovie.prepend(bestMovieTitle);
     const bestMovieDiv = document.querySelector(".best-movie");
-    const bestMovieTitle = bestMovieDiv.getElementsByTagName("h1");
-    bestMovieTitle[0].textContent = bestMovieJson.title;
     const bestMovieDescription = bestMovieDiv.getElementsByTagName("p");
     bestMovieDescription[0].textContent = bestMovieJson.description;
     const bestMovieImage = bestMovieDiv.getElementsByTagName("img");
